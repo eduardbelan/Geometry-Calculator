fa46252040ddcafc92b3d9a8dc6d08312f20e2be
Eduard Belan <eduard.belan.git@gmail.com>
2023-05-09 07:17:04 +0200

    add triangle formulas (allgemeines / unregelmäßiges Dreieck) to formulas file add input method for triangle (allgemeines / unregelmäßiges Dreieck) to calculator clean up some unnecessary code in square, and rectangle

--- formulas.py
+++ formulas.py
@@ -34,13 +34,86 @@ def gegeben_breite_umfang(b, U):
 def gegeben_laenge_umfang(a, U):
     return (U - 2 * a) / 2
 
+
 def gegeben_breite_flaecheninhalt(b, A):
     return A / b
+
 
 def gegeben_laenge_flaecheninhalt(a, A):
     return A / a
 
+
 def gegeben_umfang_flaecheninhalt(U, A):
-    a = ((U / 2) + math.sqrt((U / 2)**2 - 4*A)) / 2
+    a = ((U / 2) + math.sqrt((U / 2) ** 2 - 4 * A)) / 2
     b = A / a
     return (a, b)
+
+
+# Triangle
+# Dreiecksarten:
+#   - allgemeines/unregelmößiges Dreieck
+#   - gleichseitiges Dreieck
+#   - gleichschenkliges Dreieck
+#   - spitzwinkliges Dreieck
+#   - rechtwinkliges Dreieck
+#   - stumpfwinkliges Dreieck
+# zu berechnende Werte:
+#   - Seiten (a, b, c)
+#   - Höhen (a, b, c)
+#   - Winkel (alpha, beta, gamma)
+#   - Umfang
+#   - Fläche
+
+# allgemeines/unregelmäßiges Dreieck
+#   - basis & höhe
+#   - seite & seite & seite
+#   - seite & seite & eingeschlossener winkel
+#   - seite & seite & gegenüberliegender winkel
+def allg_BH(basis, hoehe):
+    """allgemeines Dreieck, Basis & Höhe known"""
+
+    # Länge Seiten
+    seite_a = math.sqrt(hoehe ** 2 + (0.5 * basis) ** 2)
+    seite_b = math.sqrt(hoehe ** 2 + ((basis - seite_a) / 2) ** 2)
+    seite_c = basis
+
+    # Winkel
+    winkel_a = math.degrees(math.asin(hoehe / seite_a))
+    winkel_b = math.degrees(math.asin(hoehe / seite_b))
+    winkel_c = 180 - winkel_a - winkel_b
+
+    # Umfang
+    umfang = seite_a + seite_b + seite_c
+
+    # Fläche
+    flaeche = 0.5 * basis * hoehe
+
+    # Höhen
+    hoehe_a = 2 * flaeche / basis
+    hoehe_b = 2 * flaeche / seite_b
+    hoehe_c = basis
+
+    gegeben = {
+        "Basis / Seite C": basis,
+        "Höhe C": hoehe_c
+    }
+    berechnet = {
+        "Seite A": seite_a,
+        "Seite B": seite_b,
+        "Höhe A": hoehe_a,
+        "Höhe B": hoehe_b,
+        "\u03B1": winkel_a,  # alpha
+        "\u03B2": winkel_b,  # beta
+        "\u03B3": winkel_c,  # gamma
+        "Umfang": umfang,
+        "Fläche": flaeche
+    }
+
+    print("Gegeben:")
+    for k, v in gegeben.items():
+        print(k, round(v, 2))
+    print("")
+    print("Berechnet:")
+    for k, v in berechnet.items():
+        print(k, round(v, 2))
+    print("")
--- geo-calc.py
+++ geo-calc.py
@@ -9,6 +9,9 @@ class GeoCalc:
 
     def __init__(self):
         """init some attributes if needed"""
+        self.triangle_hoehe_c = None
+        self.triangle_seite_c = None
+        self.triangle_whats_given = None
         self.pick = None
         self.square_whats_given = None
         self.square_seite = None
@@ -38,7 +41,7 @@ class GeoCalc:
         elif self.pick == 2:
             self.rectangle_input()
         elif self.pick == 3:
-            print("Triangle is calculated...")
+            self.triangle_input()
         elif self.pick == 4:
             print("Trapezoid is calculated...")
         elif self.pick == 5:
@@ -52,15 +55,12 @@ class GeoCalc:
                                 "Type a Number to pick the known value: "))
         self.square_whats_given = whats_given
 
-        if self.square_whats_given == 1:
-            seite = float(input("\nEnter Seitenlänge: "))
-            self.square_seite = seite
-        elif self.square_whats_given == 2:
-            umfang = float(input("\nEnter Umfang: "))
-            self.square_umfang = umfang
-        elif self.square_whats_given == 3:
-            flaeche = float(input("\nEnter Fläche: "))
-            self.square_flaeche = flaeche
+        if whats_given == 1:
+            self.square_seite = float(input("\nEnter Seitenlänge: "))
+        elif whats_given == 2:
+            self.square_umfang = float(input("\nEnter Umfang: "))
+        elif whats_given == 3:
+            self.square_flaeche = float(input("\nEnter Fläche: "))
         self.square_calc()
 
     def square_calc(self):
@@ -97,11 +97,14 @@ class GeoCalc:
                             "3. Umfang\n"
                             "4. Flächeninhalt\n"
                             "Type two Numbers to pick the known values in this format 'num1num2': ")
+        print("")
+
         two_picks = False
         while not two_picks:
             if len(whats_given) != 2:
                 whats_given = input("Invalid Input. "
                                     "Enter two digits with no spaces in between in this format 'num1num2': ")
+                print("")
             else:
                 PICKS_RECTANGLE.append(int(whats_given[0]))
                 PICKS_RECTANGLE.append(int(whats_given[1]))
@@ -110,17 +113,13 @@ class GeoCalc:
 
         for i in PICKS_RECTANGLE:
             if i == 1:
-                seite_a = float(input("\nEnter Seitenlänge a: "))
-                self.rectangle_seite_a = seite_a
+                self.rectangle_seite_a = float(input("Enter Seitenlänge a: "))
             elif i == 2:
-                seite_b = float(input("\nEnter Seitenlänge b: "))
-                self.rectangle_seite_b = seite_b
+                self.rectangle_seite_b = float(input("Enter Seitenlänge b: "))
             elif i == 3:
-                umfang = float(input("\nEnter Umfang: "))
-                self.rectangle_umfang = umfang
+                self.rectangle_umfang = float(input("Enter Umfang: "))
             elif i == 4:
-                flaeche = float(input("\nEnter Flächeninhalt: "))
-                self.rectangle_flaeche = flaeche
+                self.rectangle_flaeche = float(input("Enter Flächeninhalt: "))
         self.rectangle_calc()
 
     def rectangle_calc(self):
@@ -180,6 +179,33 @@ class GeoCalc:
                   f"Gegeben Fläche: {self.rectangle_flaeche}\n"
                   f"Berechnet Seitenlänge a: {self.rectangle_seite_a}\n"
                   f"Berechnet Seitenlänge b: {self.rectangle_seite_b}")
+
+    def triangle_input(self):
+        """User chooses known value, User enters known value"""
+        whats_given = int(input("Allgemeines / Unregelmäßiges Dreieck:\n"
+                                "\t1. Basis & Höhe\n"
+                                "\t2. Alle Seiten\n"
+                                "\t3. Zwei Seiten und der eingeschlossene Winkel\n"
+                                "\t4. Zwei Seiten und ein gegenüberliegender Winkel\n"
+                                "Type a Number to pick the known value: "))
+        print("")
+        self.triangle_whats_given = whats_given
+
+        if whats_given == 1:
+            self.triangle_seite_c = float(input("Enter Basis: "))
+            self.triangle_hoehe_c = float(input("Enter Höhe: "))
+        elif whats_given == 2:
+            self.triangle_seite_a = float(input("Enter Seite A: "))
+            self.triangle_seite_b = float(input("Enter Seite B: "))
+            self.triangle_seite_c = float(input("Enter Seite C: "))
+        elif whats_given == 3 or self.triangle_whats_given == 4:
+            self.triangle_seite_x = float(input("Enter Seite 1: "))
+            self.triangle_seite_y = float(input("Enter Seite 2: "))
+            self.triangle_winkel_x = float(input("Enter Winkel: "))
+        self.triangle_calc()
+
+    def triangle_calc(self):
+        """Missing values of Triangle are calculated"""
 
 
 calc = GeoCalc()
